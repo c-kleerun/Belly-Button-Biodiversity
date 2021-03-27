1851b9ffe03ce703746645d6869ba6621996718a
c-klee-run <kristy.allred@gmail.com>
2021-03-27 14:47:01 -0400

    updated files, added bubble graph function

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,6 +1,8 @@
 d3.json('../../samples.json').then((data) => {
+    
     console.log(data);
 
+// populate dropdown data
 for (var i=0; i<data.names.length; i++) {
     var option = data.names[i];
     var el = document.createElement('option');
@@ -18,38 +20,61 @@ function optionChanged () {
 
     console.log(inputValue)
 
-    // var names = [];
-    
-    // if (inputValue == inputElement)
+    return loadBar(inputValue), loadBubble(inputValue);
+};
+
+function loadBar (inputValue) {
+    d3.json('../../samples.json').then((data) => {
+        var dataFiltered = data.samples.filter((data) => {
+            return data.id === inputValue;
+        })[0]
+
+        console.log(dataFiltered)
+            var trace1 = {
+                x: dataFiltered.sample_values.slice(0, 10).reverse(),
+                y: dataFiltered.otu_ids.slice(0, 10).map((otu_id) => {
+                    return `otu ${otu_id}`
+                }).reverse(),
+                text: dataFiltered.otu_labels.slice(0, 10).reverse(),
+                type: 'bar',
+                orientation: 'h',
+                name: 'Patient Top 10 OTUs'
+            };
+        
+            var data = [trace1]
+        
+            var layout = {
+            };
+        
+            Plotly.newPlot('bar', data, layout);
+        });
     }
 
-    var trace1 = {
-        x: data.samples.map((ids) => {
-            return ids.otu_ids
-        }),
-        y: data.samples.map((values) => {
-            return values.sample_values.length
-        }),
-        type: 'bar',
-        name: 'Prevalence of OTU IDs'
-    };
-
-    // var trace2 = {
-    //     x: data.otu_ids,
-    //     y: data.sample_values,
-    //     type: 'bubble',
-    //     mode: 'markers',
-    //     marker: {
-    //         size: data.sample_values,
-    //         color: otu_ids
-    //     },
-    //     name: 'Sample Values and OTU Comparison'
-    // };
-
-    var data = [trace1]
-
-    var layout = {
-    };
-
-    Plotly.newPlot('bar', data, layout);
+    function loadBubble (inputValue) {
+        d3.json('../../samples.json').then((data) => {
+            var dataFiltered = data.samples.filter((data) => {
+                return data.id === inputValue;
+            })[0]
+    
+            console.log(dataFiltered)
+                var trace2 = {
+                    x: dataFiltered.otu_ids,
+                    y: dataFiltered.sample_values,
+                    text: dataFiltered.otu_labels,
+                    mode: 'markers',
+                    marker: {
+                        size: dataFiltered.sample_values,
+                        color: dataFiltered.otu_ids,
+                        colorscale: 'YlGnBu'
+                    },
+                };
+            
+                var data = [trace2]
+            
+                var layout = {
+                };
+            
+                Plotly.newPlot('bubble', data, layout);
+            });
+        }
 });
