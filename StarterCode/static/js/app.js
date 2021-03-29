8a8fbd5c908c7f0855b25a76b83e1ac2c68eb9b7
c-klee-run <kristy.allred@gmail.com>
2021-03-28 22:00:19 -0400

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,7 +3,7 @@ function init() {
 
         console.log(data);
 
-        // populate dropdown data
+        // populate dropdown data, set default
         for (var i = 0; i < data.names.length; i++) {
             var option = data.names[i];
             var el = document.createElement('option');
@@ -15,108 +15,134 @@ function init() {
         loadDemo(firstID)
         loadBubble(firstID)
         loadBar(firstID)
+        loadGauge(firstID)
     });
 };
 
-
+// Run all graphs on ID No. Change
 d3.selectAll('#selDataset').on('change', optionChanged);
 
-function optionChanged() {
-    d3.event.preventDefault();
-    var inputElement = d3.select('#selDataset')
-    var inputValue = inputElement.property('value');
+    function optionChanged() {
+        d3.event.preventDefault();
+        var inputElement = d3.select('#selDataset')
+        var inputValue = inputElement.property('value');
 
-    console.log(inputValue)
+        console.log(inputValue)
 
-    return loadBar(inputValue), loadBubble(inputValue), loadDemo(inputValue);
-};
+        return loadBar(inputValue), loadBubble(inputValue), loadDemo(inputValue), loadGauge(inputValue);
+    };
 
-function loadDemo(inputValue) {
-    d3.json('../../samples.json').then((data) => {
-        var metaFiltered = data.metadata.filter((data) => {
-            return data.id == inputValue;
-        })[0]
+    // Function to call Demographics info
+    function loadDemo(inputValue) {
+        d3.json('../../samples.json').then((data) => {
+            var metaFiltered = data.metadata.filter((data) => {
+                return data.id == inputValue;
+            })[0]
 
-        console.log(metaFiltered)
+            console.log(metaFiltered)
 
-        var meta = d3.select("#sample-metadata");
-        meta.selectAll("table").remove();
-        var table = meta.append("table");
-        table.append("tr").append("td").text(`id: ${inputValue}`);
-        table.append("tr").append("td").text(`ethnicity: ${metaFiltered.ethnicity}`);
-        table.append("tr").append("td").text(`gender: ${metaFiltered.gender}`);
-        table.append("tr").append("td").text(`age: ${metaFiltered.age}`);
-        table.append("tr").append("td").text(`location: ${metaFiltered.location}`);
-    });
-    // loadGauge(metaFiltered.wfreq)
-};
+            var meta = d3.select("#sample-metadata");
+            meta.selectAll("table").remove();
+            var table = meta.append("table");
+            table.append("tr").append("td").text(`id: ${inputValue}`);
+            table.append("tr").append("td").text(`ethnicity: ${metaFiltered.ethnicity}`);
+            table.append("tr").append("td").text(`gender: ${metaFiltered.gender}`);
+            table.append("tr").append("td").text(`age: ${metaFiltered.age}`);
+            table.append("tr").append("td").text(`location: ${metaFiltered.location}`);
+        });
+    };
 
+    // Function to pull data and create Gauge for wfreq 
+    function loadGauge(inputValue) {
+        d3.json('../../samples.json').then((data) => {
+            var metaFiltered = data.metadata.filter((data) => {
+                return data.id == inputValue;
+            })[0]
 
+            var data = [{ 
+                value: metaFiltered.wfreq,
+                title: { text: `Patient ${inputValue} Wash Frequency`},
+                type: 'indicator', 
+                mode: 'gauge+number', 
+                gauge: {axis: {visble: false, range: [0, 10]}}
+            }];
 
-function loadBar(inputValue) {
-    d3.json('../../samples.json').then((data) => {
-        var dataFiltered = data.samples.filter((data) => {
-            return data.id === inputValue;
-        })[0]
+            var layout = {
+                width: 600,
+                height: 500,
+                margin: { t: 0, b: 0 }
+            };
 
-        var trace1 = {
-            x: dataFiltered.sample_values.slice(0, 10).reverse(),
-            y: dataFiltered.otu_ids.slice(0, 10).map((otu_id) => {
-                return `otu ${otu_id}`
-            }).reverse(),
-            text: dataFiltered.otu_labels.slice(0, 10).reverse(),
-            type: 'bar',
-            orientation: 'h'
-        };
+            Plotly.newPlot('gauge', data, layout);
+        });
+    };
 
-        var data = [trace1]
+// Function to pull data and create bar graph for top 10 OTU values 
+    function loadBar(inputValue) {
+        d3.json('../../samples.json').then((data) => {
+            var dataFiltered = data.samples.filter((data) => {
+                return data.id === inputValue;
+            })[0]
 
-        var layout = {
-            title: `Patient ${inputValue} Top 10 OTUs`,
-            xaxis: {
-                title: 'Sample OTU Values'
-            },
-            yaxis: {
-                title: 'OTU ID'
-            }
-        };
+            var trace1 = {
+                x: dataFiltered.sample_values.slice(0, 10).reverse(),
+                y: dataFiltered.otu_ids.slice(0, 10).map((otu_id) => {
+                    return `otu ${otu_id}`
+                }).reverse(),
+                text: dataFiltered.otu_labels.slice(0, 10).reverse(),
+                type: 'bar',
+                orientation: 'h'
+            };
 
-        Plotly.newPlot('bar', data, layout);
-    });
-};
+            var data = [trace1]
 
-function loadBubble(inputValue) {
-    d3.json('../../samples.json').then((data) => {
-        var dataFiltered = data.samples.filter((data) => {
-            return data.id === inputValue;
-        })[0]
+            var layout = {
+                title: `Patient ${inputValue} Top 10 OTUs`,
+                xaxis: {
+                    title: 'Sample OTU Values'
+                },
+                yaxis: {
+                    title: 'OTU ID'
+                }
+            };
 
-        var trace2 = {
-            x: dataFiltered.otu_ids,
-            y: dataFiltered.sample_values,
-            text: dataFiltered.otu_labels,
-            mode: 'markers',
-            marker: {
-                size: dataFiltered.sample_values,
-                color: dataFiltered.otu_ids,
-                colorscale: 'Bluered'
-            },
-        };
+            Plotly.newPlot('bar', data, layout);
+        });
+    };
 
-        var data = [trace2]
+// function to pull and create bubble graph for all OTU data
+    function loadBubble(inputValue) {
+        d3.json('../../samples.json').then((data) => {
+            var dataFiltered = data.samples.filter((data) => {
+                return data.id === inputValue;
+            })[0]
 
-        var layout = {
-            title: `Patient ${inputValue} Top 10 OTUs`,
-            xaxis: {
-                title: 'OTU IDs'
-            },
-            yaxis: {
-                title: 'Sample OTU Values'
-            }
-        };
+            var trace2 = {
+                x: dataFiltered.otu_ids,
+                y: dataFiltered.sample_values,
+                text: dataFiltered.otu_labels,
+                mode: 'markers',
+                marker: {
+                    size: dataFiltered.sample_values,
+                    color: dataFiltered.otu_ids,
+                    colorscale: 'Bluered'
+                },
+            };
 
-        Plotly.newPlot('bubble', data, layout);
-    });
-};
+            var data = [trace2]
 
-init()
+            var layout = {
+                title: `Patient ${inputValue} Top 10 OTUs`,
+                xaxis: {
+                    title: 'OTU IDs'
+                },
+                yaxis: {
+                    title: 'Sample OTU Values'
+                }
+            };
+
+            Plotly.newPlot('bubble', data, layout);
+        });
+    };
+
+    init()
